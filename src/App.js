c335d7d55f2249a904f9a8ecd2d3d77daf7048ff
Anand <124582976+Anand3125@users.noreply.github.com>
2024-04-10 04:38:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import SearcIcon from "./search.svg";
 import MovieCard from "./MovieCard.jsx";
 
-const API_URL = 'http://www.omdbapi.com?apikey=${process.env.REACT_APP_MOVIE_KEY}';
+const API_URL = 'http://www.omdbapi.com?apikey=e0a6b95';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
